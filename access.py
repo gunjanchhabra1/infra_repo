373c4c7e49c3f28cec6f51c702fade0cf9750a76
ankur-cr-esper <ankur-cr@esper.io>
2023-10-04 10:26:57 +0530

    updated json_data line

--- access.py
+++ access.py
@@ -5,6 +5,6 @@ s3_client = boto3.client('s3')
 bucket_name = 'demo-bucket-acs'
 object_key = 'version.json'
 response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
-json_data = json.loads(response['Body'].read().decode('utf-8'))
+json_data = json.loads(response['Body'].read())
 access_value = json_data["image_version"]
 print(access_value)
